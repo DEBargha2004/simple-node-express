7fb953c5b0b18a350a803b06a2f956db97082c3f
DEBargha2004 <debarghasaha16@gmail.com>
2024-08-09 23:19:49 +0530

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT ?? 3000
+const UNAME = process.env.UNAME ?? 'unknown'
+
+app.get('/', (req, res) => {
+  res.status(200).send(`Hello from, ${UNAME}!`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
